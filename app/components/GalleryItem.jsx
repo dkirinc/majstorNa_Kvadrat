2a06ad20f54175f58f34d83c00a3d5717d253afc
dkirinc <dorian.kirincic@gmail.com>
2025-01-07 19:21:51 +0100

    Added overflow to gallery

--- app/components/GalleryItem.jsx
+++ app/components/GalleryItem.jsx
@@ -25,11 +25,11 @@ const GalleryItem = ({ item, admin }) => {
 
     return (
         <div className=''>
-            <div onClick={!admin ? (() => onClick()) : null} className={'relative w-[221px]  xl:w-[420px] m-1  flex ease-in-out duration-300 ' + (!admin ? ' hover:scale-95' : '')}>
+            <div onClick={!admin ? (() => onClick()) : null} className={'relative w-[341px] h-[400px]  xl:w-[420px] m-1  flex ease-in-out duration-300 ' + (!admin ? ' hover:scale-95' : '')}>
                 <div className='flex  my-1  rounded-lg '>
-                    <img src={item.pic_1} alt="" className='w-[110px] xl:w-[209px] h-[190px] xl:h-[380px] object-fill  rounded-l-md' />
+                    <img src={item.pic_1} alt="" className='w-[150px] xl:w-[209px] h-full object-fill  rounded-l-md' />
                     <div className='w-[1px] bg-orange-main opacity-80'></div>
-                    <img src={item.pic_2} alt="" className='w-[110px] xl:w-[209px] h-full object-fill rounded-r-md' />
+                    <img src={item.pic_2} alt="" className='w-[150px] xl:w-[209px] h-full object-fill rounded-r-md' />
                     {admin ? (<FaCircleXmark onClick={() => deleteItem(item.id)} className={"absolute top-0 -left-1  w-10 h-10 text-red-400 bg-white rounded-full scale-105 hover:scale-110"} />) : null}
                 </div>
             </div>
