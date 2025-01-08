be8eaaf185c82a4bebc7b8efbbea4a8d3cc2c7c8
dkirinc <dorian.kirincic@gmail.com>
2025-01-08 12:57:22 +0100

    Modal excluded from mobile view

--- app/components/GalleryItem.jsx
+++ app/components/GalleryItem.jsx
@@ -1,18 +1,37 @@
 
 import React from 'react'
+import { useState, useEffect } from 'react'
 import { FaCircleXmark } from 'react-icons/fa6'
 import { useAppContext } from '../context'
-import Image from 'next/image'
 
 const GalleryItem = ({ item, admin }) => {
 
     const { setOpen, setPictureId, setPic1, setPic2, deleteGalleryItem } = useAppContext()
 
+    const [isMobile, setIsMobile] = useState(false)
+
+    //choose the screen size 
+    const handleResize = () => {
+        if (window.innerWidth < 720) {
+            setIsMobile(true)
+        } else {
+            setIsMobile(false)
+        }
+    }
+
+    // create an event listener
+    useEffect(() => {
+        window.addEventListener("resize", handleResize)
+    })
+
+
     const onClick = () => {
-        setPictureId(item.id)
-        setPic1(item.pic_1)
-        setPic2(item.pic_2)
-        setOpen(true)
+        if (!isMobile) {
+            setPictureId(item.id)
+            setPic1(item.pic_1)
+            setPic2(item.pic_2)
+            setOpen(true)
+        }
     }
 
     const deleteItem = (id) => {
